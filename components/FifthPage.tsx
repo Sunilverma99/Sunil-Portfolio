53d7c095fca977407f513c23ad13f8d6d23b22fc
Sunil <sv784333@gmial.com>
2024-04-21 12:37:55 +0530

    fifth page complete UI

--- components/FifthPage.tsx
+++ components/FifthPage.tsx
@@ -1,27 +1,195 @@
 "use client";
+ 
+import Image from "next/image";
 import React from "react";
-import { BackgroundGradient } from "./ui/background-gradient";
-
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import Link from "next/link";
 function FifthPage() {
   return (
-    <div className="w-full h-full">
-    <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-      <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-        Air Jordan 4 Retro Reimagined
-      </p>
+    <div className="w-full bg-gray-800">
+      <h1 className="text-5xl text-center font-bold p-4 text-white">Projects</h1>
+      <div className="flex gap-12 mx-12 flex-wrap justify-center">
+      <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Blog Application
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Welcome to our blog application, where creativity meets connectivity. With intuitive design, seamless social integration, and powerful customization options, our platform empowers users to share their stories and engage with a global audience effortlessly."
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={`/blog-application.png`}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            website link →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            <Link href={'https://blog-application-o4u3.onrender.com/'}>click here</Link>
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
 
-      <p className="text-sm text-neutral-600 dark:text-neutral-400">
-        The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-        February 17, 2024. Your best opportunity to get these right now is by
-        entering raffles and waiting for the official releases.
-      </p>
-      <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-        <span>Buy now </span>
-        <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-          $100
-        </span>
-      </button>
-    </BackgroundGradient>
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Real Estate Company
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Welcome to CashShift, your premier destination for real estate solutions. With our expert agents, personalized service, and a commitment to exceeding expectations, we turn your property dreams into reality.
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={`/epicHeaven.png`}
+            height="600"
+            width="500"
+            className="h-60 w-full  object-contain rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Github link →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            <Link href={'https://github.com/Sunilverma99/EpicHeaven.git'}>click here</Link>
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Hostel Management  app
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Discover our easy-to-use hostel management app designed specifically for your college. Simplify hostel tasks, from room allocation to maintenance, for both students and administrators. Say goodbye to hassle and hello to efficiency with our intuitive solution.
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={`/Hostel Mangement app.png`}
+            height="600"
+            width="500"
+            className="h-60 w-full object-contain rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            GitHub link →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            <Link href={'https://github.com/Sunilverma99/HiveStay.git'}>click here</Link>
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          News app
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+         Stay ahead with our comprehensive news app delivering top stories in sports, business, and technology sectors. Get the latest updates and insights on sports business, tech innovations, and market trends, all in one place.
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={`/News app.png`}
+            height="600"
+            width="500"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Github link →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            <Link href={'https://github.com/Sunilverma99'}>click here</Link>
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+      </div>
+ 
   </div>
   )
 }
